e7e13f30afecd17a4143f9e2ad1a59a634a67447
Leosantosnunes <leonardon09@gmail.com>
2023-08-22 22:04:52 -0400

    First commit - Add Bio and Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BioComponent } from './bio/bio.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BioComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
